b92bf847416adec0868e27f4503ff0ad287742a3
Tofique Khan <86665480+tkwebdesigner@users.noreply.github.com>
2024-03-27 23:41:59 +0530

    Image Detail Page Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,3 +51,45 @@ window.addEventListener('scroll', function () {
     navbar.classList.remove('sticky');
   }
 });
+
+// Disable context menu
+// document.addEventListener('contextmenu', event => event.preventDefault());
+
+// Disable specific keys
+// document.onkeydown = function (e) {
+//     // Disable F12 key
+//     if (e.key === "F12" || (e.key === "F12" && e.ctrlKey === true) || (e.key === "F12" && e.shiftKey === true) || (e.key === "F12" && e.altKey === true) || (e.key === "F12" && e.metaKey === true)) {
+//         return false;
+//     }
+
+//     // Disable Ctrl+Shift+I (inspect element)
+//     if (e.ctrlKey && e.shiftKey && e.key === "I") {
+//         return false;
+//     }
+
+//     // Disable Ctrl+Shift+J (inspect element)
+//     if (e.ctrlKey && e.shiftKey && e.key === "J") {
+//         return false;
+//     }
+
+//     // Disable Ctrl+U (view page source)
+//     if (e.ctrlKey && e.key === "U") {
+//         return false;
+//     }
+// }
+
+window.addEventListener('scroll', function() {
+  var scrollTopBtn = document.getElementById('scrollTopBtn');
+  if (window.scrollY > 100) { // Adjust the scroll height to show the button
+    scrollTopBtn.classList.add('show');
+  } else {
+    scrollTopBtn.classList.remove('show');
+  }
+});
+
+document.getElementById('scrollTopBtn').addEventListener('click', function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
